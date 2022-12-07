5a4d917726454e260989d7a41dd3d63ccb400577
tstarzec <tstar391@d127.org>
2022-12-07 10:21:48 -0600

    A lot

---
+++ Unstressed/GetStartedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GetStartedViewController.swift
+//  Unstressed
+//
+//  Created by Tristan L Starzec (Student) on 12/7/22.
+//
+
+import UIKit
+
+class GetStartedViewController: UIViewController {
+        
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
